bf5e79ef3f87dac2b0057ccb4189128ef9c73eb8
Jeff <joachim_fricker@yahoo.com>
2020-01-13 22:03:00 +0100

    get rid of routers and templates this is overhead for such a simple app

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,23 +17,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/slideShow', function(req, res) {
+	console.log("value of foo is:", req.query.foo);
+    res.sendFile('slideShow.html', { root: path.join(__dirname, 'public') });
 });
 
 module.exports = app;
